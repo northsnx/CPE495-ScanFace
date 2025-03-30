0aa99ab945dd4b8b965416d22f96d7436a022692
Thanaphat Sirithep <109312299+northsnx@users.noreply.github.com>
2025-03-31 03:39:27 +0700

    Delete server directory

--- server/routers/attendanceRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {addAttendance} = require('../controllers/attendanceController')
-
-router.post("/add-attendance", addAttendance);  
-
-module.exports = router;
